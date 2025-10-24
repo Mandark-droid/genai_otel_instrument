d9cb9503699f0722c1c56d1d5e4afd452c232a4c
kshitij thakkar <89932595+Mandark-droid@users.noreply.github.com>
2025-10-24 10:53:33 +0530

    feat: Add total GPU memory capacity metric

--- genai_otel/__version__.py
+++ genai_otel/__version__.py
@@ -28,7 +28,7 @@ version_tuple: VERSION_TUPLE
 commit_id: COMMIT_ID
 __commit_id__: COMMIT_ID
 
-__version__ = version = '0.1.2.dev0'
-__version_tuple__ = version_tuple = (0, 1, 2, 'dev0')
+__version__ = version = '0.1.4.dev0'
+__version_tuple__ = version_tuple = (0, 1, 4, 'dev0')
 
-__commit_id__ = commit_id = 'g4553b2a46'
+__commit_id__ = commit_id = 'g3bf473315'
